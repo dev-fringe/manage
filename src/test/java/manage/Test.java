e5ef99f973e8ea551c3ac79ae312e33b38b6960f
LEE HAK DO <dev.fringe@gmail.com>
2018-04-03 22:50:42 +0900

    commit

--- src/test/java/manage/Test.java
+++ src/test/java/manage/Test.java
@@ -8,7 +8,7 @@ import com.thoughtworks.xstream.XStream;
 public class Test {
 	public static void main(String[] args) throws FileNotFoundException {
 		XStream xStream = new XStream();
-		xStream.processAnnotations(ComplexExample.class);
+		xStream.processAnnotations(Properties.class);
 		Object readObject = xStream.fromXML(new File("pns-ums-bizMatrix.xml"));
 		System.out.println("Object loaded by xstream: " + readObject);
 	}
